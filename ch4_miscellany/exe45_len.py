2190e38d7f0d077384b7d97f4ebe72db4330702a
girdeux31 <mesado31@gmail.com>
2024-08-31 01:30:17 +0200

    exe45 variants

---
+++ ch4_miscellany/exe45_len.py
@@ -0,0 +1,30 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from foo import get_collatz_reduction
+
+
+print('Exercise 4.5')
+
+file = 'exe45_len.png'
+max_number = 10**4
+color_map = 'plasma'
+y_max = 300
+size = 4
+numbers = []
+colors = []
+
+cmap = mpl.colormaps[color_map]
+
+for a in range(2, max_number+1):
+
+    maximum = len(get_collatz_reduction(a))
+    numbers.append(maximum)
+    colors.append(cmap(maximum/y_max))
+
+plt.scatter(range(2, max_number+1), numbers, s=size, c=colors)
+plt.gca().set_ylim([0, y_max])
+plt.xlabel('a')
+plt.ylabel('Max Collatz of a')
+plt.savefig(file)
+plt.show()
